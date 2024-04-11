8c1ca7b19ffaad2a1a7555f0db74cde8d0dd5567
checamar0 <checamar02@gmail.com>
2024-04-11 15:31:35 +0300

    Init commit

---
+++ tests/test_selene.py
@@ -0,0 +1,18 @@
+from selene import be, by
+from selene.support.shared import browser
+from selene.support.shared.jquery_style import s
+
+
+def test_github():
+    browser.open('https://github.com')
+    # browser.config.window_width = 1500
+    # browser.config.window_height = 900
+    s(".header-search-button").click()
+    s("#query-builder-test").send_keys("eroshenkoam/allure-example")
+    s("#query-builder-test").submit()
+
+    s(by.link_text("eroshenkoam/allure-example")).click()
+
+    s("#issues-tab").click()
+
+    s(by.partial_text("#76")).should(be.visible)
